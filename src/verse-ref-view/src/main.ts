2cdc79be87690fab8b85fb8799ff5b8b083d9336
tjcouch-sil <tj_couch@sil.org>
2024-04-17 18:15:27 -0500

    Added verse ref view functionality

--- src/verse-ref-view/src/main.ts
+++ src/verse-ref-view/src/main.ts
@@ -1,7 +1,89 @@
-import { logger } from '@papi/backend';
+import papi, { logger } from '@papi/backend';
+import {
+  ExecutionActivationContext,
+  GetWebViewOptions,
+  IWebViewProvider,
+  SavedWebViewDefinition,
+  WebViewDefinition,
+} from '@papi/core';
+import verseRefWebView from './verse-ref.web-view?inline';
+import { getWebViewTitle } from './utils';
 
-export async function activate() {
+const verseRefWebViewType = 'verseRefView.webView';
+
+interface VerseRefViewOptions extends GetWebViewOptions {
+  projectId: string | undefined;
+}
+
+/**
+ * Function to prompt for a project and open it in the verse ref view. Registered as a command
+ * handler.
+ */
+async function openVerseRefView(projectId: string | undefined): Promise<string | undefined> {
+  let projectIdForWebView = projectId;
+  if (!projectIdForWebView) {
+    projectIdForWebView = await papi.dialogs.selectProject({
+      title: 'Select Project to open with Verse Ref View',
+      prompt: 'Choose the project to open with Verse Ref View:',
+      includeProjectTypes: '^ParatextStandard$',
+    });
+  }
+  if (projectIdForWebView) {
+    const options: VerseRefViewOptions = { projectId: projectIdForWebView };
+    return papi.webViews.getWebView(verseRefWebViewType, undefined, options);
+  }
+  return undefined;
+}
+
+/** Simple web view provider that provides verse ref web views when papi requests them */
+const verseRefWebViewProvider: IWebViewProvider = {
+  async getWebView(
+    savedWebView: SavedWebViewDefinition,
+    getWebViewOptions: VerseRefViewOptions,
+  ): Promise<WebViewDefinition | undefined> {
+    if (savedWebView.webViewType !== verseRefWebViewType)
+      throw new Error(
+        `${verseRefWebViewType} provider received request to provide a ${savedWebView.webViewType} web view`,
+      );
+
+    // We know that the projectId (if present in the state) will be a string.
+    const projectId =
+      getWebViewOptions.projectId ||
+      // eslint-disable-next-line no-type-assertion/no-type-assertion
+      (savedWebView.state?.projectId as string) ||
+      undefined;
+    const projectsMetadata = projectId
+      ? await papi.projectLookup.getMetadataForProject(projectId)
+      : undefined;
+    return {
+      title: getWebViewTitle(projectsMetadata?.name),
+      ...savedWebView,
+      content: verseRefWebView,
+      state: {
+        ...savedWebView.state,
+        projectId,
+      },
+    };
+  },
+};
+
+export async function activate(context: ExecutionActivationContext): Promise<void> {
   logger.info('verse-ref-view is activating!');
+
+  const openVerseRefViewPromise = papi.commands.registerCommand(
+    'verseRefView.open',
+    openVerseRefView,
+  );
+
+  const verseRefWebViewProviderPromise = papi.webViewProviders.register(
+    verseRefWebViewType,
+    verseRefWebViewProvider,
+  );
+
+  // Await the registration promises at the end so we don't hold everything else up
+  context.registrations.add(await verseRefWebViewProviderPromise, await openVerseRefViewPromise);
+
+  logger.info('verse-ref-view is finished activating!');
 }
 
 export async function deactivate() {
--- src/verse-ref-view/src/types/verse-ref-view.d.ts
+++ src/verse-ref-view/src/types/verse-ref-view.d.ts
@@ -1,4 +1,14 @@
-declare module 'verse-ref-view' {
-  // Add extension types exposed on the papi for other extensions to use here
-  // More instructions can be found in the README
+declare module 'verse-ref-view' {}
+
+declare module 'papi-shared-types' {
+  export interface CommandHandlers {
+    /**
+     * Opens a new Verse Ref Web View and returns the Web View id
+     *
+     * @param projectId Optional project ID of the project to open. Prompts the user to select a
+     *   project if not provided
+     * @returns WebView id for new Verse ref WebView or `undefined` if the user canceled the dialog
+     */
+    'verseRefView.open': (projectId: string | undefined) => Promise<string | undefined>;
+  }
 }
