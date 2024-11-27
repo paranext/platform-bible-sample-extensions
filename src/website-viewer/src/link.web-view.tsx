872f7f49c0682a52f97c8dae188d98caf53591dd
Sebastian-ubs <slingk@biblesocieties.org>
2024-11-27 16:02:54 +0100

    moving vars out of react function to stay const

--- src/website-viewer/src/link.web-view.tsx
+++ src/website-viewer/src/link.web-view.tsx
@@ -2,10 +2,11 @@ import { WebViewProps } from '@papi/core';
 import { useLocalizedStrings } from '@papi/frontend/react';
 import { LocalizeKey } from 'platform-bible-utils';
 
+const descriptionTextL10nKey = '%websiteViewerMenu_clickLink%';
+const localizedStringKeys: LocalizeKey[] = [descriptionTextL10nKey];
+
 // misusing the title to pass around the url, because I cannot come up with an easy way to get it in
 global.webViewComponent = function LinkWebView({ title }: WebViewProps) {
-  const descriptionTextL10nKey = '%websiteViewerMenu_clickLink%';
-  const localizedStringKeys: LocalizeKey[] = [descriptionTextL10nKey];
   const [{ [descriptionTextL10nKey]: descriptionTextLocalized }] =
     useLocalizedStrings(localizedStringKeys);
   return (
