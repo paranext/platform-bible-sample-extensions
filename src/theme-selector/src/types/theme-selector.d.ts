86f857c31700f4215feb7cb8fc8070f40da0ce79
TJ Couch <104016682+tjcouch-sil@users.noreply.github.com>
2025-04-30 08:47:07 -0500

    Added first pass theme selector WebView (#34)

--- src/theme-selector/src/types/theme-selector.d.ts
+++ src/theme-selector/src/types/theme-selector.d.ts
@@ -1,4 +1,15 @@
-declare module 'theme-selector' {
-  // Add extension types exposed on the papi for other extensions to use here
-  // More instructions can be found in the README
+declare module 'theme-selector' {}
+
+declare module 'papi-shared-types' {
+  export interface CommandHandlers {
+    /**
+     * Opens a new Theme Selector WebView and returns the Web View id
+     *
+     * @param projectId Optional project ID of the project to open. Prompts the user to select a
+     *   project if not provided
+     * @returns WebView id for new Theme Selector WebView or `undefined` if the user canceled the
+     *   dialog
+     */
+    'themeSelector.open': () => Promise<string | undefined>;
+  }
 }
